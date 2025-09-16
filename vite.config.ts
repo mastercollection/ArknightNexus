a9a20712db79958c2ea6358215e0cd8a1367ad50
ZeroSugar <blkd1310@gmail.com>
2025-09-16 22:22:14 +0900

    Vue3 + Tauri Init

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const host =
+  typeof process !== "undefined" ? process.env.TAURI_DEV_HOST : undefined;
+const PLATFORM = process.env.TAURI_ENV_PLATFORM;
+
+// https://vite.dev/config/
+export default defineConfig(({
+  plugins: [vue()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 3001,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  build: {
+	outDir: "./dist",
+	target: PLATFORM === "windows" ? "chrome107" : "safari16",
+	minify: !process.env.TAURI_DEBUG ? false : "esbuild",
+	emptyOutDir: true,
+    chunkSizeWarningLimit: 1024,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+}));
